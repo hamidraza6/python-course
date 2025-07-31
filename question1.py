66b7adecf40b4e3f1a9905ef0be2ca17d7f0f60a
hamidraza6 <hamidraza71450@gmail.com>
2025-07-31 07:59:28 +0500

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,5 @@
+first_name=input("enter your first name:")
+print("length of your name is",len(first_name))
+str="hi i am $ string $ "
+print(" no of time coming dollar $ sign in str ")
+print(str.count("$"))
